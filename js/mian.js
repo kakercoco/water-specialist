eb879494cbe50e0c8a00c50c6bd6e94c19594c4b
许天星 <kaker.xu@71360.com>
2018-07-10 16:32:11 +0800

    企业主页

---
+++ js/mian.js
@@ -0,0 +1,25 @@
+function changeTab(val, e) {
+	$(e).parent().children().removeClass('active')
+	$(e).addClass('active')
+	$('.new-produce-left ul').hide()
+	var ul = '.ul-' + val
+	$('.new-produce-left').children(ul).show()
+}
+function changeNav(val, e) {
+	$(e).parent().find('li').removeClass('active')
+	$(e).addClass('active')
+	$('.new-produce').hide()
+	selected = '.new-produce-' + val
+	$(selected).show()
+}
+$('.hot-produce-left .right li').click(function(){
+	$(this).parent().find('div').hide()
+	$(this).next().show()
+})
+function changeCompany(val, e) {
+	$(e).parent().find('li').removeClass('active')
+	$(e).addClass('active')
+	$('.brand-ul').hide()
+	selected = '.brand-ul-' + val
+	$(selected).show()
+}
