348974eed13ba60be5ae64cf2a377018c8ff66e2
许天星 <kaker.xu@71360.com>
2018-07-11 18:15:34 +0800

    企业展示，咨询主页，咨询列表

--- js/main.js
+++ js/main.js
@@ -27,3 +27,19 @@ $('.show-more').click(function(){
 	$(this).prev().css('height','auto')
 	$(this).hide()
 })
+$('.company-detail-img-left li img').click(function(e){
+	$('.company-detail-img-left>img').attr('src',e.target.src)
+	$('.company-detail-img-left li').removeClass('active')
+	$(this).parent().addClass('active')
+})
+ var mySwiper = new Swiper ('.setion-one .swiper-container', {
+	loop: true,
+	pagination: {
+    el: '.setion-one .swiper-pagination',
+         clickable: true,
+      },
+  })
+ $('.infor-section-three-left .right li').click(function(){
+	$(this).parent().find('div').hide()
+	$(this).next().show()
+})
